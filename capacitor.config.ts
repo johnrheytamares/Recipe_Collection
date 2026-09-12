e0085f2688a097de54c4aa72ce74b9484e3814af
johnrheytamares <johnrheynedamotamares@gmail.com>
2026-09-12 15:33:48 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Recipe_Collection',
+  webDir: 'dist'
+};
+
+export default config;
